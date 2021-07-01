7ccf40039111276b3321dfe1f7babfea3c9ab977
hero1601 <86766004+hero1601@users.noreply.github.com>
2021-07-01 12:24:02 +0530

    Add files via upload

---
+++ back-end/Interface/ICodeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codes;
+
+namespace Terminology.Interface
+{
+    public interface ICodeRepository
+    {
+        AllinfoProvide GetAllCode(CodeSystemProvide csp);
+        InfoProvide AddCode(InfoProvide ip);
+        AllinfoProvide FindCode(CodeProvide cp);
+        InfoProvide UpdateCode(InfoProvide ip);
+        InfoProvide DeleteCode(CodeProvide cp);
+    }
+}
---
+++ back-end/Interface/IValueSetRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Valuesets;
+
+namespace Terminology.Interface
+{
+    public interface IValueSetRepository
+    {
+        AllValueSetinfoProvide FindValueSet(ValueSetURIVersionProvide vsvp);
+        AllValueSetinfoProvide AddValueSet(AllValueSetinfoProvide vsvp);
+        ValueSetToCodes FindCodesFromValueSet(ValueSetURIVersionProvide vsvp);
+    }
+}
